af58ff4d4345e176c2c945bbacee704452fbcac2
aytea14 <muhdamar4687@gmail.com>
2023-12-04 09:41:45 +0800

    Update Main.java

--- src/main/java/dev/inctrl/retailer/Main.java
+++ src/main/java/dev/inctrl/retailer/Main.java
@@ -3,6 +3,6 @@ package dev.inctrl.retailer;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World by InCTRL.dev");
     }
 }
