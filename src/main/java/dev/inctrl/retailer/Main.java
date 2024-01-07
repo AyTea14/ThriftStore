86fc68c412080be47cbea92239922a3ad9bec055
aytea14 <muhdamar4687@gmail.com>
2024-01-07 23:51:53 +0800

    Updates

--- src/main/java/dev/inctrl/retailer/Main.java
+++ src/main/java/dev/inctrl/retailer/Main.java
@@ -11,10 +11,70 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World by InCTRL.dev");
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to In CTRL Store.");
+        System.out.println("Now enter all the required informations.");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Are you a member? [Yes/No]: ");
+        boolean isMember = sc.nextLine().equalsIgnoreCase("yes");
+        System.out.print("How many items you have?: ");
+        int amount = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("\nThe price of the item is based on the ID's prefix. Here the list");
+        System.out.println("A: RM 70\nB: RM 55\nC: RM 35\nD: RM 20\n");
+
+        String[] items = new String[amount];
+
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Enter your item no. " + (i + 1));
+            System.out.print("Item Name: ");
+            String itemName = sc.nextLine();
+            System.out.print("Item ID: ");
+            String id = sc.nextLine();
+
+            items[i] = String.format("[%s] %s", id, itemName);
+        }
+        sc.close();
+
         readFile(membersFile, members);
+        members.add(String.format("Name: %s, Member: %s", name, isMember ? "Yes" : "No"));
         saveToFile(membersFile, members);
 
+        System.out.println("\nHere is your reciept.");
+        calcTotalPrice(items);
     }
+
+    private static void calcTotalPrice(String[] items) {
+        double totalPrice = 0;
+        for (int i = 0; i < items.length; i++) {
+            double price = 0;
+            String currentItem = items[i];
+            String id = currentItem.split(" ")[0].replace("[", "").replace("]", "");
+            id = id.toLowerCase();
+
+            if (id.startsWith("a")) {
+                price = 70.0;
+            } else if (id.startsWith("b")) {
+                price = 55.0;
+            } else if (id.startsWith("c")) {
+                price = 35.0;
+            } else if (id.startsWith("d")) {
+                price = 20.0;
+            }
+            totalPrice += price;
+
+            System.out.printf("%s: %.2f%n", currentItem, price);
+        }
+
+        System.out.printf("Total Price: RM %.2f%n", totalPrice);
+    }
+
+    // private static String getMember(String name) {
+        
+    // }
 
     private static void saveToFile(String filePath, List<String> data) {
         try {
@@ -53,4 +113,5 @@ public class Main {
 
         return content.toString();
     }
+
 }
